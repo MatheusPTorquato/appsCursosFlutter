c182e3346308bf52a3e3e976bf897d02b42aba01
MatheusPTorquato <dev.matheusptorquato@gmail.com>
2024-08-19 15:49:38 -0300

    Atualização app 02

--- app02_jamilton/lib/aulas.dart
+++ app02_jamilton/lib/aulas.dart
@@ -1,24 +1,42 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-      title: "Frases do dia",
-      /*home: Column(children: <Widget>[
-      Text("User"),
-      Text("Senha"),
-    ],),*/
-      home: Container(
-        //color: Colors.white,
-        decoration: BoxDecoration(
-          border: Border.all(width: 3, color: Colors.white)
+  runApp(const HomePage());
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Tester"),
+          backgroundColor: Colors.blueAccent,
         ),
-        child: const Row(
-          children: <Widget>[
-            Text("Texto")
-          ],
+        body: const Padding(
+          padding: EdgeInsets.all(50),
+          child: Row(
+            children: [
+              Text("Teste"),
+            ],
+          ),
         ),
-      )
-      //home: Container(color: Colors.white,),
-      ));
+        bottomNavigationBar: const BottomAppBar(
+          color: Colors.blueAccent,
+          child: Padding(
+            padding: EdgeInsets.all(16),
+            child: Row(
+              children: [
+                Text("Texto 1"),
+                Text("Texto 2"),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
 }
